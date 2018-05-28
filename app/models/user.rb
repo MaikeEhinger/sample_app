7ae30a65cfa53e1d6fb7e8bdab9c7c6d3308a16e
Maike Ehinger <maike@lionizers.com>
2018-05-28 09:16:11 +0200

    extended password

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ before_save { email.downcase! }
   validates :email, presence: true, length: { maximum: 255 },
                   format: { with: VALID_EMAIL_REGEX },
                   uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
